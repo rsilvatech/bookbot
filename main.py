bb806b5ac587b9ec0c8b0d2fef6e726812b43060
AnotherDev <rafaelsilvahome@gmail.com>
2026-01-01 18:05:29 -0300

    Added a count character function on stats.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 #bookbot - Programa em Python que analisa arquivos de texto e imprime um relatório estatístico do uso de palavras e caracteres encontrados neles.
 
-from stats import count_book_words
+from stats import count_book_words, count_book_characters
 
 path_to_file = './books/frankenstein.txt'
 
 def main():
     print(f"Found {count_book_words(path_to_file)} total words")
+    print(count_book_characters(path_to_file))
     
-
 main()
--- stats.py
+++ stats.py
@@ -13,3 +13,19 @@ def count_book_words(path_to_file):
 
     return num_words
 
+def count_book_characters(path_to_file):
+    # Turns a book.txt file into a big string (book_as_string) 
+    with open(path_to_file) as book:
+        book_as_string = book.read()
+
+    #counts the amount of characters inside book_as_string an make a pair of character-->quantity inside the num_each_character dictionary
+    num_each_character = {}
+    for character in book_as_string:
+        character = character.lower()
+        if (character in num_each_character):
+            num_each_character[character] += 1
+        else:
+            num_each_character[character] = 1
+    
+    return num_each_character
+        
