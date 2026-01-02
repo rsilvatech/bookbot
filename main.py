a1fa2f9d68712e77aeb0238c2dc25c784e2502ce
AnotherDev <rafaelsilvahome@gmail.com>
2026-01-01 22:59:53 -0300

    Added command line sorted character output, but path_to_file still hardcoded

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 #bookbot - Programa em Python que analisa arquivos de texto e imprime um relatório estatístico do uso de palavras e caracteres encontrados neles.
 
-from stats import count_book_words, count_book_characters
+import stats, sys
 
-path_to_file = './books/frankenstein.txt'
+path_to_file = "./books/frankenstein.txt"
+
+sorted_character_dict = stats.sort_character_count(path_to_file)
 
 def main():
-    print(f"Found {count_book_words(path_to_file)} total words")
-    print(count_book_characters(path_to_file))
-    
+    print("============ BOOKBOT ============")
+    print(f"Analysing book found at {path_to_file}...")
+    print("----------- Word Count ----------")
+    print(f"Found {stats.count_book_words(path_to_file)} total words")
+    print("--------- Character Count -------")
+    for pair in sorted_character_dict:
+        print(f"{pair['name']}: {pair['num']}")
+    print("============= END ===============")
+
 main()
+
--- stats.py
+++ stats.py
@@ -19,13 +19,35 @@ def count_book_characters(path_to_file):
         book_as_string = book.read()
 
     #counts the amount of characters inside book_as_string an make a pair of character-->quantity inside the num_each_character dictionary
-    num_each_character = {}
+    character_count = {}
     for character in book_as_string:
         character = character.lower()
-        if (character in num_each_character):
-            num_each_character[character] += 1
+        if (character in character_count):
+            character_count[character] += 1
         else:
-            num_each_character[character] = 1
+            character_count[character] = 1
     
-    return num_each_character
-        
+    return character_count
+
+def sort_on(items):
+    return items["num"]
+
+def sort_character_count(path_to_file):
+    character_count = count_book_characters(path_to_file)
+    each_character_num = []
+    for character in character_count:
+        if (character.isalpha() == True): 
+            name_num_pair = {}
+            name = character
+            num = character_count[character]
+            name_num_pair["name"] = name
+            name_num_pair["num"] = num
+            each_character_num.append(name_num_pair)
+            each_character_num.sort(reverse=True, key=sort_on)
+        else:
+            pass
+    return each_character_num     
+
+    
+
+               
